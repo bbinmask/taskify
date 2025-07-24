68215d9f99036d95a9dbdfe342a1b411631facd6
BB in Mask <bbinmask@gmail.com>
2025-07-24 15:32:07 +0530

    Day 5

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,32 @@
+import { db } from "@/lib/db";
+
 const OrganizationIdPage = ({ params }: { params: { id: string } }) => {
-  return <div>Organization Page</div>;
+  async function create(formData: FormData) {
+    "use server";
+
+    const title = formData.get("title") as string;
+
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  }
+
+  return (
+    <div>
+      <form action={create}>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          placeholder="Title"
+          className="border-black border p-1"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;

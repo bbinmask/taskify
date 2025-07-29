926e4a2e64b79486978c83061862a7a56d124786
BB in Mask <bbinmask@gmail.com>
2025-07-29 16:30:32 +0530

    List header is completed

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/ListHeader.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/ListHeader.tsx
@@ -1,12 +1,25 @@
 "use client";
 
 import { List } from "@/generated/prisma";
-
+import { useState, useRef, ComponentRef } from "react";
 interface ListHeaderProps {
   data: List;
 }
 
 const ListHeader = ({ data }: ListHeaderProps) => {
+  const [title, setTitle] = useState(data.title);
+  const [isEditing, setIsEditing] = useState(false);
+  const formRef = useRef<ComponentRef<"form">>(null);
+  const inputRef = useRef<ComponentRef<"input">>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
   return (
     <div className="pt-2 px-2 text-sm font-semibold flex justify-between items-start gap-x-2">
       <div className="w-full text-sm px-2.5 py-1 h-7 border-transparent">

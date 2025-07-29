d8b00efd8e1cab407b741307be9dffb46a0b61b4
BB in Mask <bbinmask@gmail.com>
2025-07-29 15:40:28 +0530

    Day 8

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/ListForm.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/ListForm.tsx
@@ -1,9 +1,115 @@
 "use client";
 
+import { useState, useRef, ComponentRef } from "react";
+import { useParams, useRouter } from "next/navigation";
+import ListWrapper from "./ListWrapper";
+import { Plus, X } from "lucide-react";
+import { useEventListener, useOnClickOutside } from "usehooks-ts";
+import FormInput from "@/components/forms/form-input";
+import FormSubmit from "@/components/forms/form-submit";
+import { Button } from "@/components/ui/button";
+
+import { createList } from "@/actions/create-list";
+import { useAction } from "@/hooks/use-action";
+import { toast } from "sonner";
+
 interface ListFormProps {}
 
 const ListForm = ({}: ListFormProps) => {
-  return <div>ListForm</div>;
+  const params = useParams();
+  const router = useRouter();
+  const [isEditing, setIsEditing] = useState(false);
+  const formRef = useRef<ComponentRef<"form">>(null);
+  const inputRef = useRef<ComponentRef<"input">>(null);
+
+  const enableEditing = () => {
+    setIsEditing(true);
+
+    setTimeout(() => {
+      inputRef.current?.focus();
+    });
+  };
+
+  const disableEditing = () => {
+    setIsEditing(false);
+  };
+
+  const { execute, fieldErrors } = useAction(createList, {
+    onSuccess: (data) => {
+      toast.success(`List "${data.title}" created`);
+      disableEditing();
+      router.refresh();
+    },
+
+    onError: (err) => {
+      toast.error(err || "Could not create lise.");
+    },
+  });
+
+  const onKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Escape") {
+      disableEditing();
+    }
+  };
+
+  useEventListener("keydown", onKeyDown);
+  useOnClickOutside(formRef as any, disableEditing);
+
+  const onSubmit = (formData: FormData) => {
+    const title = formData.get("title") as string;
+    const boardId = formData.get("boardId") as string;
+
+    execute({
+      title,
+      boardId,
+    });
+  };
+
+  if (isEditing) {
+    return (
+      <ListWrapper>
+        <form
+          ref={formRef}
+          action={onSubmit}
+          className="w-full p-3 rounded-md bg-white space-y-4 shadow-md"
+        >
+          <FormInput
+            ref={inputRef}
+            id="title"
+            errors={fieldErrors}
+            className="text-sm px-2 py-1 h-7 border-transparent hover:border-input focus:border-input transition"
+            placeholder="Enter a title..."
+          />
+          <input
+            type="text"
+            readOnly
+            className="hidden"
+            value={params.boardId}
+            name="boardId"
+          />
+          <div className="flex items-center gap-x-1">
+            <FormSubmit>Add List</FormSubmit>
+            <Button onClick={disableEditing} size={"sm"} variant={"ghost"}>
+              <X className="h-5 w-5" />
+            </Button>
+          </div>
+        </form>
+      </ListWrapper>
+    );
+  }
+
+  return (
+    <ListWrapper>
+      <button
+        onClick={enableEditing}
+        type="button"
+        className="w-full text-black rounded-md bg-white/80 hover:bg-white/50 transition p-3 flex items-center text-sm"
+      >
+        <Plus />
+        Add a list
+      </button>
+    </ListWrapper>
+  );
 };
 
 export default ListForm;

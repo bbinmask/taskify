edee4c9e9da496171f566fa30ef07b5d47e0549e
BB in Mask <bbinmask@gmail.com>
2025-07-30 14:59:26 +0530

    Card components finished.

--- src/app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_components/ListItem.tsx
@@ -4,6 +4,8 @@ import { ComponentRef, useRef, useState } from "react";
 import { ListWithCards } from "@/types/type";
 import ListHeader from "./ListHeader";
 import CardForm from "./CardForm";
+import { cn } from "@/lib/utils";
+import CardItem from "./CardItem";
 
 interface ListItemProps {
   data: ListWithCards;
@@ -27,6 +29,17 @@ const ListItem = ({ data, index }: ListItemProps) => {
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
         <ListHeader onAddCard={enableEditing} data={data} />
+
+        <ol
+          className={cn(
+            "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
+            data.cards.length > 0 ? "mt-2" : "mt-0"
+          )}
+        >
+          {data.cards.map((card, i) => (
+            <CardItem data={card} index={i} key={card.id} />
+          ))}
+        </ol>
 
         <CardForm
           ref={textareaRef}

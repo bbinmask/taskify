cfa849404904c26986ba68d6d199d7861ef6bbdc
BB in Mask <bbinmask@gmail.com>
2025-07-31 14:21:56 +0530

    Creating new modal for audit log

---
+++ src/lib/create-audit-log.ts
@@ -0,0 +1,35 @@
+import { ACTION, ENTITY_TYPE } from "@/generated/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { db } from "./db";
+
+interface Props {
+  entityId: string;
+  entityType: ENTITY_TYPE;
+  entityTitle: string;
+  action: ACTION;
+}
+
+export const createAuditLog = async (props: Props) => {
+  try {
+    const { orgId } = await auth();
+    const user = await currentUser();
+
+    if (!orgId || !user) {
+      throw new Error("User not found");
+    }
+
+    const { entityId, entityTitle, entityType, action } = props;
+
+    await db.auditLog.create({
+      data: {
+        orgId,
+        entityId,
+        entityTitle,
+        entityType,
+        action,
+      },
+    });
+  } catch (error: any) {
+    console.log("AUDIT LOG ERROR: ", error?.message);
+  }
+};

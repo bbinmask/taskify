406148d0ea4959b5900fed4f309adba8a54d08ae
BB in Mask <bbinmask@gmail.com>
2025-08-01 09:28:48 +0530

    Fixed info component

--- src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -2,10 +2,12 @@ import { Separator } from "@/components/ui/separator";
 import Info from "./_components/Info";
 import BoardList from "./_components/BoardList";
 import { Suspense } from "react";
+import { checkSubscription } from "@/lib/subscription";
 const OrganizationIdPage = async ({ params }: { params: { id: string } }) => {
+  const isPro = await checkSubscription();
   return (
     <div className="w-full mb-20">
-      <Info />
+      <Info isPro={isPro} />
       <Separator className="mb-2" />
 
       <div className="px-2 md:px-4">
